1978a96b226d104029b3eaf9a08459ada02ce5ec
Jacob O'Riordan-Ross <57334647+JacobORiordanRoss@users.noreply.github.com>
2019-12-01 18:12:39 -0500

    Add files via upload

--- Arduino_to_Excel_via_PLXDAQ_Bluetooth/Arduino_to_Excel_via_PLXDAQ_Bluetooth.ino
+++ Arduino_to_Excel_via_PLXDAQ_Bluetooth/Arduino_to_Excel_via_PLXDAQ_Bluetooth.ino
@@ -7,6 +7,10 @@ SoftwareSerial bt(2,3);  // (RX,TX)
 #include <SPI.h>
 #define SensorPin0 A0 
 float sensorValue0 = 0; 
+float pressureValue;
+float maxSaturation;
+float memFactor;
+int count = 0;
 #include <Adafruit_BMP280.h> //Pressure & Temperature Sensor Library
 #define BMP_SCK (13)  
 #define BMP_MISO (12)
@@ -37,10 +41,18 @@ void loop() {
    sensorValue0 = sensorValue0 + analogRead(SensorPin0); 
    delay(1); 
  } 
+ 
+if (bmp.readTemperature() < -5) {
+  bt.println("Temperature is sufficiently low. Flooding potential is undeterminable based on soil water holding capacity data.");
+}
+else {
  sensorValue0 = sensorValue0/800.0;
- pressureValue = bmp.readPressure();
- maxSaturation = 24.799*pow(pressureValue, -0.141);
+ pressureValue = (bmp.readPressure());
+ maxSaturation = 24.799*pow(pressureValue/100000, -0.141);
  memFactor = sensorValue0/maxSaturation*100;
+}
+//insert code here to describe the change from below to above freezing, indicates melting snow or ice
+ 
 bt.print("DATA,TIME,");
 bt.print(sensorValue0);
 bt.print(",");
@@ -48,5 +60,15 @@ bt.print(pressureValue);
 bt.print(",");
 bt.println(memFactor);
 delay(1000);
- 
+
+if (memFactor < 100) {
+  count = 0;
+}
+else{
+  count++;  //add code here to add to a count if Mem is greater than 100 for 5 consecutive hours
+}
+
+if (count >= 5*3600) {  //CHANGE THIS TO 20 FOR THE DEMO  
+  bt.println("WARNING: SOIL SATURATION CAPACITY AT MAXIMUM. POTENTIAL FOR INCOMING FLOOD.");
+}
 }
